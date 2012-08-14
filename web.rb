6e747dc251cf53a7ea4feb52553ed1dae362f177
Sam McLaughlin <sammclaughlin@flocoapps.com>
2012-08-13 22:32:13 -0400

    Added json gem to Gemfile, made a new attempt to return json from the service.

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ get '/genderid/:name' do
   gender = `echo #{params[:name].capitalize} | awk -f gender.awk`
   status 200
   content_type :json
-  { :name => params[:name].capitalize, :gender => gender }.to_json
+  { :gender => gender, :name => params[:name].capitalize }.to_json
 end
 
 
