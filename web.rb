4d032c0ae235179209d88b15f459b21d7ed6a2ca
Sam McLaughlin <sammclaughlin@flocoapps.com>
2012-08-13 22:08:00 -0400

    attempted fix for route error

--- web.rb
+++ web.rb
@@ -1,9 +1,9 @@
+require 'rubygems'
 require 'sinatra'
-require "sinatra/json"
 
 get '/genderid/:name' do
-  status 200
-  body(`echo Ricky | awk -f gender.awk`.to_json)
+  gender = `echo #{params[:name]} | awk -f gender.awk`
+  gender
 end
 
 
