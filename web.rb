73ed7023011480ca3212d7cbd752700007068d10
Sam McLaughlin <sammclaughlin@flocoapps.com>
2012-08-13 22:15:55 -0400

    Return is json

--- web.rb
+++ web.rb
@@ -3,7 +3,9 @@ require 'sinatra'
 
 get '/genderid/:name' do
   gender = `echo #{params[:name].capitalize} | awk -f gender.awk`
-  gender
+  status 200
+  body(gender.to_json)
+  gender.to_json
 end
 
 
