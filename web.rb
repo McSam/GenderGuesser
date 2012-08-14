a4c459c311989e9086026cd4c20c772f1520499c
Sam McLaughlin <sammclaughlin@flocoapps.com>
2012-08-13 22:12:56 -0400

    Added in capitalization for strings, now any case will work

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/genderid/:name' do
-  gender = `echo #{params[:name]} | awk -f gender.awk`
+  gender = `echo #{params[:name].capitalize} | awk -f gender.awk`
   gender
 end
 
